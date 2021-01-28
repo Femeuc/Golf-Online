55cc818830e2f4e4e7c6fd3f9bc7b319387324f2
Femeuc <femeuf5@gmail.com>
2021-01-28 18:28:27 -0300

    Script para atingir o objetivo dessa branch: fazer o setup do PUN 2

---
+++ Assets/Scripts/Launcher.cs
@@ -0,0 +1,72 @@
+﻿using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+
+namespace Com.Femeuc.GolfOnline
+{
+    public class Launcher : MonoBehaviourPunCallbacks
+    {
+        private string gameVersion = "1";
+        [Tooltip("The maximum number of players per room. When a room is full, it can't be joined by new players, and so new room will be created")]
+        [SerializeField]
+        private byte maxPlayersPerRoom = 4;
+        void Awake()
+        {
+            // this makes sure we can use PhotonNetwork.LoadLevel() on the master client and all clients in the same room sync their level automatically
+            PhotonNetwork.AutomaticallySyncScene = true;
+        }
+        void Start()
+        {
+        }
+
+        public void Connect()
+        {
+            // we check if we are connected or not, we join if we are , else we initiate the connection to the server.
+            if (PhotonNetwork.IsConnected)
+            {
+                // #Critical we need at this point to attempt joining a Random Room. If it fails, we'll get notified in OnJoinRandomFailed() and we'll create one.
+                PhotonNetwork.JoinRandomRoom();
+            }
+            else
+            {
+                // #Critical, we must first and foremost connect to Photon Online Server.
+                PhotonNetwork.ConnectUsingSettings();
+                PhotonNetwork.GameVersion = gameVersion;
+            }
+        }
+
+        public override void OnConnectedToMaster()
+        {
+            PhotonNetwork.JoinRandomRoom();
+            printPlayersInRoomEveryFourSeconds();
+        }
+
+
+        public override void OnDisconnected(DisconnectCause cause)
+        {
+            Debug.LogWarningFormat("PUN Basics Tutorial/Launcher: OnDisconnected() was called by PUN with reason {0}", cause);
+        }
+
+        public override void OnJoinRandomFailed(short returnCode, string message)
+        {
+            // #Critical: we failed to join a random room, maybe none exists or they are all full. No worries, we create a new room.
+            PhotonNetwork.CreateRoom(null, new RoomOptions { MaxPlayers = maxPlayersPerRoom });
+        }
+
+        public override void OnJoinedRoom()
+        {
+            Debug.Log("PUN Basics Tutorial/Launcher: OnJoinedRoom() called by PUN. Now this client is in a room.");
+        }
+
+        public void printPlayersInRoomEveryFourSeconds()
+        {
+            int index = 1;
+            foreach(Player player in PhotonNetwork.PlayerList)
+            {
+                Debug.Log("Player " + index + ": " + player.NickName);
+                index++;
+            }
+            Invoke("printPlayersInRoomEveryFourSeconds", 4);
+        }
+    }
+}
