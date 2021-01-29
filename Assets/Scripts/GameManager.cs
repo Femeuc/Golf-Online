64ed579a3fb03b95f847e5e86a4a91b7d2643233
Femeuc <femeuf5@gmail.com>
2021-01-29 17:26:07 -0300

    Criando GameManager prefab e o seu script

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine.SceneManagement;
+using Photon.Pun;
+
+
+namespace Com.Femeuc.GolfOnline
+{
+    public class GameManager : MonoBehaviourPunCallbacks
+    {
+        public override void OnLeftRoom()
+        {
+            SceneManager.LoadScene(0);
+        }
+
+        public void LeaveRoom()
+        {
+            PhotonNetwork.LeaveRoom();
+        }
+    }
+}
