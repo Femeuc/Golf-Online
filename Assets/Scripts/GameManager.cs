2d58cbff3da40ee78a8a2b6192c739d17ccc12c9
Femeuc <femeuf5@gmail.com>
2021-01-29 18:36:56 -0300

    Continuando o trabalho no GameManager

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,11 +1,26 @@
 ﻿using UnityEngine.SceneManagement;
 using Photon.Pun;
-
+using UnityEngine;
 
 namespace Com.Femeuc.GolfOnline
 {
     public class GameManager : MonoBehaviourPunCallbacks
     {
+        [Tooltip("The prefab to use for representing the player")]
+        public GameObject playerPrefab;
+
+        private void Start()
+        {
+            if (playerPrefab == null)
+            {
+                Debug.LogError("<Color=Red><a>Missing</a></Color> playerPrefab Reference. Please set it up in GameObject 'Game Manager'", this);
+            }
+            else
+            {
+                // we're in a room. spawn a character for the local player. it gets synced by using PhotonNetwork.Instantiate
+                PhotonNetwork.Instantiate(this.playerPrefab.name, new Vector3(0f, 5f, 0f), Quaternion.identity, 0);
+            }
+        }
         public override void OnLeftRoom()
         {
             SceneManager.LoadScene(0);
