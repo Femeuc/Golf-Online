960264119832834c15ed29fd32fa6f530093b74f
Femeuc <femeuf5@gmail.com>
2021-01-30 19:31:47 -0300

    Criei o script de movimento do player e adicionei no seu prefab.

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,31 @@
+﻿using Photon.Pun;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerMovement : MonoBehaviourPun
+{
+    private Rigidbody playerRb;
+    private Camera cam;          // the ball must be thrown to the forward direction of the camera
+    private Button throwBallButton;
+    
+    void Start()
+    {
+        playerRb = GetComponent<Rigidbody>();
+        cam = Camera.main;
+
+        throwBallButton = GameObject.Find("ThrowBallButton").GetComponent<Button>();
+        throwBallButton.onClick.AddListener(ThrowBall);
+    }
+
+    void Update()
+    {
+        
+    }
+
+    private void ThrowBall()
+    {
+        if (!photonView.IsMine) return; 
+
+        playerRb.AddForce(cam.transform.forward * 100, ForceMode.Force);
+    }
+}
