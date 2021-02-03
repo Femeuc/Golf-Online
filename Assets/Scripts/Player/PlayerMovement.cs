1a7eb6357f2cd912520e1519d3fa16418ef78588
Femeuc <femeuf5@gmail.com>
2021-02-03 17:01:43 -0300

    A força aplicada na bola é calculada com base nos dois scrollbars da GUI: um é para a força para frente e o outro é para a força para cima.

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -7,7 +7,9 @@ public class PlayerMovement : MonoBehaviourPun
     private Rigidbody playerRb;
     private Camera cam;          // the ball must be thrown to the forward direction of the camera
     private Button throwBallButton;
-    
+    private Scrollbar upForceScrollbar;
+    private Scrollbar forwardForceScrollBar;
+
     void Start()
     {
         if (!photonView.IsMine)
@@ -20,6 +22,9 @@ public class PlayerMovement : MonoBehaviourPun
 
         throwBallButton = GameObject.Find("ThrowBallButton").GetComponent<Button>();
         throwBallButton.onClick.AddListener(ThrowBall);
+
+        upForceScrollbar = GameObject.Find("UpForceScrollbar").GetComponent<Scrollbar>();
+        forwardForceScrollBar = GameObject.Find("ForwardForceScrollbar").GetComponent<Scrollbar>();
     }
 
     void Update()
@@ -29,6 +34,11 @@ public class PlayerMovement : MonoBehaviourPun
 
     private void ThrowBall()
     {
-        playerRb.AddForce(cam.transform.forward * 100, ForceMode.Force);
+        int upForce = (int) (upForceScrollbar.value * 10 - upForceScrollbar.value);
+        int forwardForce = (int) (forwardForceScrollBar.value * 10 - forwardForceScrollBar.value);
+
+        // Applies forwardForce and upForce to the Rigidbody
+        // Quarternion.Euler is necessary so the ball is thorwn only forward, not upward, even if the camera is pointing upward.
+        playerRb.AddForce(Quaternion.Euler(0, cam.transform.rotation.eulerAngles.y, 0) * Vector3.forward * forwardForce * 200  +  Vector3.up * upForce * 200, ForceMode.Force);
     }
 }
