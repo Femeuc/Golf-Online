2401b3b19606cbe783c469a93ff0a998cfd95143
Femeuc <femeuf5@gmail.com>
2021-02-12 11:53:43 -0300

    Criei a pasta GUI para organizar melhor os scripts

---
+++ Assets/Scripts/GUI/OnStartButtonClicked.cs
@@ -0,0 +1,19 @@
+﻿using Photon.Pun;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OnStartButtonClicked : MonoBehaviour
+{
+    private Button startButton;
+    // Start is called before the first frame update
+    void Start()
+    {
+        startButton = GetComponent<Button>();
+        startButton.onClick.AddListener(onStartButtonClicked);
+    }
+
+    private void onStartButtonClicked()
+    {
+        PhotonNetwork.LoadLevel("Room-1");
+    }
+}
