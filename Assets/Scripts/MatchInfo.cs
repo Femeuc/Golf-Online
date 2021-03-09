8db463899c89fc7de6ca6acc99cb3d8e97bd8ade
Femeuc <femeuf5@gmail.com>
2021-03-09 15:10:18 -0300

    Criei o script MatchInfo e o associei ao player prefab

---
+++ Assets/Scripts/MatchInfo.cs
@@ -0,0 +1,41 @@
+﻿using Photon.Pun;
+using Photon.Realtime;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MatchInfo : MonoBehaviour, IPunObservable
+{
+
+    int myNumberTest;
+    int theirNumberTest = -1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (PhotonNetwork.IsMasterClient)
+            myNumberTest = 1;
+        else
+            myNumberTest = 5;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log("My number is : " + myNumberTest);
+        Debug.Log("Their number is : " + theirNumberTest);
+    }
+
+    public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+    {
+        if (stream.IsWriting)
+        {
+            // We own this player: send the others our data
+            stream.SendNext(myNumberTest);
+        }
+        else
+        {
+            // Network player, receive data
+            this.theirNumberTest = (int)stream.ReceiveNext();
+        }
+    }
+}
